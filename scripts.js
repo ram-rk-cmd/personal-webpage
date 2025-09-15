3a77e495801c2ab119a8e956c8d6759c2159ce18
ram-rk-cmd <ramkumar2info@gmail.com>
2025-09-15 16:08:04 +0530

    new file:   .vscode/settings.json 	modified:   about.html 	modified:   achievements.html 	modified:   contact.html
    	modified:   index.html
    	modified:   project.html
    	modified:   scripts.js

--- scripts.js
+++ scripts.js
@@ -5,9 +5,9 @@
         var s = document.querySelector(".nav_links");
         s.classList.contains("open") ? (s.classList.remove("open"),
         e.setAttribute("aria-expanded", "false"),
-        e.src = "/images/menu.svg") : (s.classList.add("open"),
+        e.src = "images/menu.svg") : (s.classList.add("open"),
         e.setAttribute("aria-expanded", "true"),
-        e.src = "/images/close.svg")
+        e.src = "images/close.svg")
     }
     ))
 }
